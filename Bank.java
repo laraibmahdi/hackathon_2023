aa1a95ddbcee4bc4b0db29d90fe655269a36419d
laraibmahdi <85030568+laraibmahdi@users.noreply.github.com>
2023-02-25 15:46:46 -0600

    Update Bank.java

--- Bank.java
+++ Bank.java
@@ -1,41 +1,56 @@
+import com.company.Encrypter;
+
+import java.util.List;
 import java.util.*;
-public class Bank {
+import java.util.ArrayList;
 
-    private String bankName;
-    private String bankAddress;
-    private String swiftCode;
-    private int bankID;
-    private List<Account> accounts;
+    public class Bank {
 
-
-    public Bank(String name, String address, String swift, int id){
-        this.bankName = name;
-        this.bankAddress = address;
-        this.swiftCode = swift;
-        this.bankID = id;
-        this.accounts = new ArrayList<>();
-
-    }
+        String bankName;
+        String bankAddress;
+        String swiftCode;
+        int bankID;
+        List<Account> accounts;
 
 
-    public void addAccount(Account c){
-        this.accounts.add(c);
-    }
+        public Bank(String name, String address, String swift, int id){
+            this.bankName = name;
+            this.bankAddress = address;
+            this.swiftCode = swift;
+            this.bankID = id;
+            this.accounts = new ArrayList<>();
+
+        }
 
 
-    public boolean isCustomer(String name){
-        for (Account customer : accounts){
-            if (Account.getUserID().equals(name))
+        public boolean addAccount(Account c){
+            if (accounts.contains(c)){
+                System.out.println("Error, you already have an account at this institution, failed to create new account.");
+                return false;
+            }
+
+//            c = Encrypter.AES.encrypt(c);
+            this.accounts.add(c);
+
             return true;
         }
-    return false;
-    }
 
 
-    public Account getCustomer(String name){
-        Account c = null;
+        public boolean isCustomer(String name){
+            for (Account customer : accounts){
+                if (customer.getUserID().equals(name))
+                    return true;
+            }
+            return false;
+        }
 
-        return c;
-    }
-    
+        public Account getAccount(String ID){
+            for(Account customer : accounts){
+                if(customer.id.equals(ID)){
+                    return customer;
+                }
+            }
+            return null;
+        }
 }
+
