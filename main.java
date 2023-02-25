20f26a5960d514fa629d705bd6c3f4acd73c6aaf
Quan Pham <59396984+minhquanpham@users.noreply.github.com>
2023-02-25 17:36:11 -0600

    Add files via upload

--- main.java
+++ main.java
@@ -19,6 +19,65 @@ public class main {
             if (scotia.isCustomer(ID)) {
                 System.out.println("Enter your password");
                 password = sc.nextLine();
+                if(scotia.getAccount(ID).checkPass(password)){
+                    System.out.println(scotia.getAccount(ID).toString());
+                    int option;
+                    while (true) {
+                        System.out.println("1. View account balance");
+                        System.out.println("2. Deposit value");
+                        System.out.println("3. Withdraw value");
+                        System.out.println("4. Change account password");
+                        System.out.println("0. Quit");
+                        System.out.print("Please choose an option: ");
+                        option = sc.nextInt();
+                        
+                        if (option == 0)
+                            break;
+                        switch (option) {
+                            case 1:// view account balance
+                                System.out.println("Account balance: " + scotia.getAccount(ID).holderBalance);
+                                System.out.println("\n");
+                                break;
+                            case 2:// deposit
+                                double depositValue;
+                                System.out.println("Enter deposit amount: ");
+                                depositValue = sc.nextDouble();
+                                scotia.getAccount(ID).deposit(depositValue);
+                                System.out.println("new account balance: " + scotia.getAccount(ID).holderBalance);
+                                System.out.println("\n");
+                                break;
+                            case 3:// withdraw
+                                double withdrawValue;
+                                System.out.println("Enter withdraw amount: ");
+                                withdrawValue = sc.nextDouble();
+                                scotia.getAccount(ID).withdraw(withdrawValue);
+                                System.out.println("new account balance: " + scotia.getAccount(ID).holderBalance);
+                                System.out.println("\n");
+                                break;
+                            case 4:// Change password
+                                String currentpassWord = " ";
+                                String newPassword = " ";
+                                Scanner newScan = new Scanner(System.in);
+        
+                                System.out.println("Please type in your current password: ");
+                                currentpassWord = newScan.nextLine();
+                                if(!currentpassWord.equals(password)){
+                                    System.out.println("Incorrect password input!");
+                                    continue;
+                                }
+                                System.out.println("Please type in your new password: ");
+                                newPassword = newScan.nextLine();
+                                while (!scotia.getAccount(ID).changePassword(newPassword)) {
+                                    System.out.println("Please type in your new password: ");
+                                    newPassword = newScan.nextLine();
+                                }
+                                password = newPassword;
+                                break;
+                        }
+                    }
+                } else{
+                    System.out.println("Wrong Password");
+                }
             }
             // decrypt the account info
         } else { // he is not a customer
