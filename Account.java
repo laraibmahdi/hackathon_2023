cddb8187ca2d3257283064da68d2f37ad96659d8
ArqamAdiyan <75317921+ArqamAdiyan@users.noreply.github.com>
2023-02-25 20:02:31 -0600

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,81 +1,71 @@
 class Account {
     //Fields
-        private String customerName; 
-        private String id;
-        private String address;
-        private String phoneNumber;
-        private String holderPassword;
-        private double holderBalance;
+    String customerName;
+    String id;
+    String address;
+    String phoneNumber;
+    String holderPassword;
+    double holderBalance;
 
     //Constructor
-    Account(String holderName, String id, String holderPassword, String holderAddress, String holderPhoneNumber){
+    Account(String holderName,String id, String holderPassword, String holderAddress,double balance, String holderPhoneNumber){
         this.customerName = holderName;
         this.id = id;
         this.address = holderAddress;
         this.phoneNumber = holderPhoneNumber;
         this.holderPassword = holderPassword;
-        this.holderBalance = 0.0;
+        this.holderBalance = balance;
     }
 
     public String getUserID(){
-        return id;
+        return this.id;
     }
-
 
     //deposit method
     public void deposit(double amount){
         this.holderBalance += amount;
     }
-    
+
     //withdraw method
     public void withdraw(double amount){
         this.holderBalance -= amount;
     }
 
     //setPassword method
-    public void setPassword(String password){
+    private void setPassword(String password){
         holderPassword = password;
+    }
+
+    public boolean checkPassword(String pass){
+        if (pass.equals(this.holderPassword)){
+            return true;
+        }
+        return false;
+
     }
 
     //changePassword method
     public boolean changePassword(String newPassword){
-        boolean passVerify = true;
+        boolean passVerify = false;
         //verify old password
-            if(holderPassword.equals(newPassword)){
-                System.out.printf("The new password is the same as the old one! Please use a different password\n");
-                passVerify = false;
-            }
-            else{
-                //change the password
-                this.holderPassword = newPassword;
-                passVerify = true;
-                System.out.println("Password changed successfully!\n");
-            }
+        if(holderPassword.equals(newPassword)){
+            System.out.printf("The new password is the same as the old one! Please use a different password\n");
+            passVerify = false;
+        }
+        else{
+            //change the password
+            this.holderPassword = newPassword;
+            passVerify = true;
+            System.out.println("Password changed successfully!\n");
+        }
         return passVerify;
     }
 
+    public String toString(){
 
-    public boolean checkPassword(String pass){
-        return holderPassword.equals(pass);
+        String s = "";
+        s+= customerName + " " + address + " " + phoneNumber + " " + holderBalance;
+        return s;
     }
 
-    public String getName() {
-        return this.customerName;
-    }
-
-    public String getPassword() {
-        return this.holderPassword;
-    }
-
-    public String getAddress() {
-        return this.address;
-    }
-
-    public String getPhoneNumber() {
-        return this.phoneNumber;
-    }
-
-    public double getBalance(){
-        return this.holderBalance;
-    }
 }
