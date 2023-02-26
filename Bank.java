1a466c4e73cb7ff18322f9f99ccd47aaba2abce4
ArqamAdiyan <75317921+ArqamAdiyan@users.noreply.github.com>
2023-02-25 20:02:09 -0600

    Update Bank.java

--- Bank.java
+++ Bank.java
@@ -1,65 +1,70 @@
+import com.company.Encrypter;
+
+import java.util.List;
 import java.util.*;
-import java.io.*;
+import java.util.ArrayList;
 
-    public class Bank {
+public class Bank {
 
-        String bankName;
-        String bankAddress;
-        String swiftCode;
-        int bankID;
-        List<Account> accounts;
-
-
-        public Bank(String name, String address, String swift, int id){
-            this.bankName = name;
-            this.bankAddress = address;
-            this.swiftCode = swift;
-            this.bankID = id;
-            this.accounts = new ArrayList<>();
-
-        }
-
-        public boolean addAccount(Account c) {
-            if (accounts.contains(c)) {
-                System.out.println("Error, you already have an account at this institution, failed to create new account.");
-                return false;
-            }
-        
-            try {
-                final String secretKey = "scotia";
-                FileWriter fileWriter = new FileWriter("filename.txt", true);
-                fileWriter.write("\n"); // Add a blank line before writing the new account information
-                fileWriter.write(Encrypter.AES.encrypt(c.getUserID(), secretKey) + "\n");
-                fileWriter.write(Encrypter.AES.encrypt(c.getName(), secretKey) + "\n");
-                fileWriter.write(Encrypter.AES.encrypt(c.getPassword(), secretKey) + "\n");
-                fileWriter.write(Encrypter.AES.encrypt(c.getAddress(), secretKey) + "\n");
-                fileWriter.write(Encrypter.AES.encrypt(c.getPhoneNumber(), secretKey) + "\n");
-                fileWriter.close();
-            } catch (IOException e) {
-                System.out.println(e.getMessage());
-                return false;
-            }
-        
-            this.accounts.add(c);
-            return true;
-        }
-        
+    String bankName;
+    String bankAddress;
+    String swiftCode;
+    int bankID;
+    List<Account> accounts;
 
 
-        public boolean isCustomer(String name){
-            for (Account customer : accounts){
-                if (customer.getUserID().equals(name))
-                    return true;
-            }
+    public Bank(String name, String address, String swift, int id){
+        this.bankName = name;
+        this.bankAddress = address;
+        this.swiftCode = swift;
+        this.bankID = id;
+        this.accounts = new ArrayList<>();
+
+    }
+
+
+    public boolean addAccount(Account c){
+        if (accounts.contains(c)){
+            System.out.println("Error, you already have an account at this institution, failed to create new account.");
             return false;
         }
 
-        public Account getAccount(String ID){
-            for(Account customer : accounts){
-                if(customer.getUserID().equals(ID)){
-                    return customer;
-                }
-            }
-            return null;
+//            c = Encrypter.AES.encrypt(c);
+        this.accounts.add(c);
+
+        return true;
+    }
+
+
+    public boolean isCustomer(String name){
+        for (Account customer : accounts){
+            if (customer.getUserID().equals(name))
+                System.out.println(customer.getUserID());
+            return true;
         }
+        return false;
+    }
+
+    public Account getAccount(String ID){
+        for(Account customer : accounts){
+            if(customer.id.equals(ID)){
+                return customer;
+            }
+        }
+        return null;
+    }
+
+    public String writeFile(){
+        String allData="";
+        for (int i=0; i<accounts.size();i++){
+            allData+=Encrypter.AES.encrypt(accounts.get(i).id,"scotia")+"\n"+Encrypter.AES.encrypt(
+                    accounts.get(i).customerName, "scotia")+"\n"+Encrypter.AES.encrypt(accounts.get(i).
+                    holderPassword,"scotia") +"\n"
+                    +Encrypter.AES.encrypt(accounts.get(i).address, "scotia")+"\n"+
+                    Encrypter.AES.encrypt(String.valueOf(accounts.get(i).holderBalance),"scotia")+"\n"+
+                    Encrypter.AES.encrypt(accounts.get(i).phoneNumber,"scotia");
+        }
+
+        return allData;
+    }
 }
